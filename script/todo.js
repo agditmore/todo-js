574a03ef6ec9049e3fe788622a48fdd393c59a4e
agditmore <49666381+agditmore@users.noreply.github.com>
2019-05-10 06:01:29 -0500

    Show List
    for loop to create new HTML elements, assign attributes, and append each list item

--- script/todo.js
+++ script/todo.js
@@ -4,3 +4,25 @@ let todos = [
   {id: 3, text: "Get oil change", complete: false},
   {id: 4, text: "Write thank-you notes", complete: false},
 ];
+
+function showList () {
+  var entireList = document.getElementbyId("main-todo-list");
+  for (var i=0; i++; i<todos.length){
+    //creating new HTML for each item in the array
+    var newListDiv = document.createElement("div");
+    var newListCheckbox = document.createElement("input");
+    var newListItem = document.createElement("span");
+
+    //assigning attributes to new HTML
+    newListDiv.className = "todo";
+    newListCheckbox.className = "todo-checkbox";
+    newListCheckbox.type = "checkbox";
+    newListItem.className = "todo-text";
+    newListItem.innerHTML = todos[i].text;
+
+    //actually adding in new HTML
+    entireList.appendChild(newListDiv);
+    newListDiv.appendChild(newListCheckbox);
+    newListDiv.appendChild(newListItem);
+  }
+};
